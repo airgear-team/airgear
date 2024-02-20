5cf2b9945f47555ca2c2c86e1d758a759aa1f863
Oleksandr Tuleninov <talextu@gmail.com>
2024-02-20 10:33:27 +0200

    - added GoodsCategory enum; - added GoodsCategory to the Goods - added category to the goods table in db.changelog-master.xml

---
+++ src/main/java/com/airgear/model/GoodsCategory.java
@@ -0,0 +1,23 @@
+package com.airgear.model;
+
+/**
+ * The {@code GoodsCategory} enumeration represents different categories of goods.
+ * Each constant corresponds to a specific category, and can be used to classify goods in a system.
+ * <p>
+ *
+ * @author Oleksandr Tuleninov
+ * @version 01
+ */
+public enum GoodsCategory {
+
+    REAL_ESTATE,
+    TRANSPORT_AND_SPECIAL_EQUIPMENT,
+    TECH_AND_ELECTRONICS,
+    CLOTHING_AND_ACCESSORIES,
+    RECREATION_EQUIPMENT,
+    TOOLS_AND_EQUIPMENT,
+    SPORTS_EQUIPMENT,
+    BEAUTY_AND_HEALTH,
+    EVENT_GOODS
+
+}
