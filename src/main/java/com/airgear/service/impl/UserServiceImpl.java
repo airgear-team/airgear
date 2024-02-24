4593110fa3df0480f70f8a0ab30bbd95cb849beb
Ilya Deus <deus.ilya@gmail.com>
2024-02-24 23:35:41 +0300

    fix changelog liq

--- src/main/java/com/airgear/service/impl/UserServiceImpl.java
+++ src/main/java/com/airgear/service/impl/UserServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Set;
 
 import com.airgear.exception.ForbiddenException;
-import com.airgear.model.AccountStatus;
 import com.airgear.repository.AccountStatusRepository;
 import com.airgear.repository.UserRepository;
 import com.airgear.model.Role;
