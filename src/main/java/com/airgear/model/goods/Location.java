ec3f7637b86973fe6f43c96bd36b45a6d6fecb17
Alexandre Ilchenko <alexilchenko2005@gmail.com>
2024-02-26 16:06:25 +0200

    Edited Location model to add foreign keys. Created LocationRepository

--- src/main/java/com/airgear/model/goods/Location.java
+++ src/main/java/com/airgear/model/goods/Location.java
@@ -2,17 +2,19 @@ package com.airgear.model.goods;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Entity
 public class Location {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-    private final String region;
     private final String settlement;
+    private Long regionId;
 
 }
