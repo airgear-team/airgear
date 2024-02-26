068e4b7dc8288c3f68472a2ec69a34a219560611
Alexandre Ilchenko <alexilchenko2005@gmail.com>
2024-02-26 14:17:49 +0200

    Edited Location model

--- src/main/java/com/airgear/model/goods/Location.java
+++ src/main/java/com/airgear/model/goods/Location.java
@@ -2,11 +2,17 @@ package com.airgear.model.goods;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 public class Location {
 
-    private final String katottg;
-    private final String settlement;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
     private final String region;
+    private final String settlement;
 
 }
