4bf6b1c1dc4c18815f8fd07f4cd8f9142efb5888
Suttori <Suttori8@gmail.com>
2024-03-06 13:27:35 +0200

    rework EmailController

--- src/main/java/com/airgear/controller/EmailController.java
+++ src/main/java/com/airgear/controller/EmailController.java
@@ -20,10 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RestController
@@ -35,30 +31,6 @@ public class EmailController {
 
     @PostMapping("/send")
     public String sendEmail(@RequestBody EmailsRequestStructure request) {
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-        AtomicInteger counter = new AtomicInteger(0);
-        AtomicInteger dailyCounter = new AtomicInteger(0);
-        try {
-            for (String address : request.getAddresses()) {
-                if (dailyCounter.get() >= 1500) {
-                    //TODO save unsent emails
-                    return "Limit emails per day.";
-                }
-                executorService.schedule(() -> {
-                    try {
-                        emailService.sendMail(address, request.getEmailMessage());
-                        log.info("The email was sent successfully to address: {}", address);
-                    } catch (Exception e) {
-                        log.error("Unable to send email to address: {}", address, e);
-                    }
-                }, counter.getAndIncrement(), TimeUnit.SECONDS);
-            }
-            return "All emails were submitted for sending.";
-        } catch (Exception e) {
-            log.error("Unable to submit emails for sending.", e);
-            throw new RuntimeException("Unable to send emails.");
-        } finally {
-            executorService.shutdown();
-        }
+        return emailService.sendMail(request.getEmailMessage(), request.getAddresses());
     }
 }
