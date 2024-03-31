c2b689c734203d2eb0391329e1c28a2d9024a156
Vitalii Kalinchyk <karlkory@gmail.com>
2024-03-31 20:21:10 +0300

    Moved saveGoodsView method to GoodsViewService

--- src/main/java/com/airgear/service/impl/GoodsViewServiceImpl.java
+++ src/main/java/com/airgear/service/impl/GoodsViewServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 
-@Service
+@Service("goodsViewService")
 @AllArgsConstructor
 public class GoodsViewServiceImpl implements GoodsViewService {
 
