92ae33da8b6ede516589d8257834b636f893a666
Oleksandr Tuleninov <talextu@gmail.com>
2024-07-22 22:05:34 +0300

    - added checking for location name length

--- src/main/java/com/airgear/exception/LocationException.java
+++ src/main/java/com/airgear/exception/LocationException.java
@@ -6,7 +6,11 @@ import org.springframework.web.server.ResponseStatusException;
 public class LocationException {
 
     public static ResponseStatusException locationNotFound(Integer locationId) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Location with id " + locationId + " not found");
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Location with id '" + locationId + "' not found");
     }
 
+    public static ResponseStatusException invalidLocationName(String name) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                "The location name '" + name + "' must consist of at least 3 characters.");
+    }
 }
--- src/main/java/com/airgear/service/impl/LocationServiceImpl.java
+++ src/main/java/com/airgear/service/impl/LocationServiceImpl.java
@@ -2,12 +2,14 @@ package com.airgear.service.impl;
 
 import com.airgear.dto.LocationResponse;
 import com.airgear.dto.RegionResponse;
+import com.airgear.exception.LocationException;
 import com.airgear.mapper.LocationMapper;
 import com.airgear.mapper.RegionMapper;
 import com.airgear.repository.LocationRepository;
 import com.airgear.repository.RegionsRepository;
 import com.airgear.service.LocationService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,8 +17,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class LocationServiceImpl implements LocationService {
+
+    @Value("${location.length}")
+    private String locationLength;
 
     private final LocationRepository locationRepository;
     private final RegionsRepository regionsRepository;
@@ -33,6 +38,7 @@ public class LocationServiceImpl implements LocationService {
     @Override
     @Transactional(readOnly = true)
     public Page<LocationResponse> getAllLocationsByName(Pageable pageable, String name) {
+        if (name.length() < Integer.parseInt(locationLength)) throw LocationException.invalidLocationName(name);
         return locationRepository.findAllBySettlementContainingIgnoreCase(pageable, name)
                 .map(locationMapper::toLocationResponse);
     }
