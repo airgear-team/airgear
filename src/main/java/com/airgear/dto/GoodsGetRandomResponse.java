eb39f3c0d11ad74f877ede47dcd45856cc4ab846
vitaliiSh <vitalii.shkar@gmail.com>
2024-08-12 10:24:50 +0300

    Added goods images to GoodsGetRandomResponse.

--- src/main/java/com/airgear/dto/GoodsGetRandomResponse.java
+++ src/main/java/com/airgear/dto/GoodsGetRandomResponse.java
@@ -1,11 +1,13 @@
 package com.airgear.dto;
 
 import com.airgear.model.GoodsCondition;
+import com.airgear.model.GoodsImages;
 import com.airgear.model.GoodsStatus;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -18,4 +20,5 @@ public class GoodsGetRandomResponse {
     private OffsetDateTime createdAt;
     private GoodsCondition goodsCondition;
     private GoodsStatus status;
+    private List<GoodsImages> images;
 }
